e2a368ed533eac3a099e791e22b8fcfeaafe2350
zvodd <zv.odd.101@gmail.com>
2021-03-11 03:04:20 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { SvgBarRender, SvgGrad } from "./components/svgbar";
+import { Gradient } from "./components/grad";
+
+export default function App() {
+  const grad = Gradient.fromHexs(["#34b4eb", "#c778e3", "#22eeFF"]);
+
+  return (
+    <div className="App">
+      <h1>Gradient Grinder</h1>
+      <h2>¯\_(ツ)_/¯</h2>
+      <div id="main">
+        <SvgBarRender gradient={grad} />
+        <pre>{JSON.stringify(grad, null, 2)}</pre>
+        <pre>{JSON.stringify(grad.asStops(), null, 2)}</pre>
+      </div>
+    </div>
+  );
+}
